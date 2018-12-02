ecf37dc1eff1c4dcbad3de846bf27aa9919a732d
bogdanovdya <40484332+bogdanovdya@users.noreply.github.com>
2018-12-02 22:54:05 +0500

    еще запрос и тестовый IO

--- indexApplicaton.py
+++ indexApplicaton.py
@@ -31,6 +31,11 @@ class Application:
         return cmp_list
 
     def get_data(self, cmp_ids):
+        """
+        по массиву айдишников компаний возвращает всякую дичь по ним
+        :param cmp_ids: list
+        :return: list
+        """
         cmp_list = self.bx24.call('crm.company.list', {'ORDER': {'ID': 'asc'}}, {'FILTER': {'ID': cmp_ids}},
                                   {'SELECT': ['TITLE', 'COMPANY_TYPE', 'INDUSTRY', 'REVENUE', 'EMPLOYEES']})
 
@@ -44,10 +49,25 @@ class Application:
                                       {'SELECT': ['DATE_BILL', 'DATE_PAYED', 'DATE_PAY_BEFORE', 'PRICE', 'PAYED',
                                                   'STATUS_ID', 'UF_COMPANY_ID']})
 
+        quote_list = self.bx24.call('crm.quote.list', {'ORDER': {'COMPANY_ID': 'asc'}},
+                                    {'FILTER': {'COMPANY_ID': cmp_ids}},
+                                    {'SELECT': ['COMPANY_ID', 'CLOSED', 'CLOSEDATE', 'DATE_CREATE', 'DEAL_ID',
+                                                'OPPORTUNITY', 'STATUS_ID']})
+
         cmp_df = pd.DataFrame(cmp_list['result'])
         deal_df = pd.DataFrame(deal_list['result'])
         inv_df = pd.DataFrame(invoice_list['result'])
-        ret_df = cmp_df.merge(deal_df, left_on='ID', right_on='COMPANY_ID', how='outer')
-        ret_df = ret_df.merge(inv_df, left_on='COMPANY_ID', right_on='UF_COMPANY_ID', how='outer')
-        print(ret_df.head())
-        return [cmp_list['result'], deal_list['result'], invoice_list['result']]
+        quote_df = pd.DataFrame(quote_list['result'])
+        #ret_df = cmp_df.merge(deal_df, left_on='ID', right_on='COMPANY_ID', how='outer')
+        #ret_df = ret_df.merge(inv_df, left_on='COMPANY_ID', right_on='UF_COMPANY_ID', how='outer')
+        print(cmp_list)
+        print('------------------------------------------------------------------------------------------------')
+        print(cmp_df.head())
+        print('------------------------------------------------------------------------------------------------')
+        print(deal_df.head())
+        print('------------------------------------------------------------------------------------------------')
+        print(inv_df.head())
+        print('------------------------------------------------------------------------------------------------')
+        print(quote_df.head())
+        #qwer = ret_df.as_matrix()
+        return [cmp_list['result'], deal_list['result'], invoice_list['result'], quote_list['result']]
